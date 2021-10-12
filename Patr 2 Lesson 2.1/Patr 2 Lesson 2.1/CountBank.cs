db11a37eff89e5751c0d951e3cd0e0d8f2e4ad68
distorition <85959781+distorition@users.noreply.github.com>
2021-10-12 11:14:07 +0300

    Add files via upload

---
+++ Patr 2 Lesson 2.1/Patr 2 Lesson 2.1/CountBank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patr_2_Lesson_2._1
+{
+    class CountBank
+    {
+       
+        private decimal NumCount { get; set; }
+        private decimal Balance { get; set; }
+        enum TypeCount
+        {
+            
+            settlement,
+            subsetlement,
+            credit,
+            current
+        }
+
+        public void TakeInfo()
+        {
+            Random random = new Random();
+            Console.WriteLine($"ваш номер счёта №{NumCount}, ваш баланс{Balance} ");
+            TypeCount count = (TypeCount)random.Next(1, 4);
+            Console.WriteLine($" ваш тип счёта {count}");
+        }
+        public void AddBalce(int value)
+        {
+            Balance = value;
+            
+        }
+        
+    }
+}
---
+++ Patr 2 Lesson 2.1/Patr 2 Lesson 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Patr_2_Lesson_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountBank countBank = new CountBank();
+            countBank.AddBalce(982);
+            countBank.TakeInfo();
+        }
+    }
+}
