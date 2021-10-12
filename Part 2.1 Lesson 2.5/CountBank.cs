8cca4321d668ca4f30f6b1aecf7c5b2509a74a49
distorition <85959781+distorition@users.noreply.github.com>
2021-10-12 12:43:16 +0300

    Add files via upload

---
+++ Part 2.1 Lesson 2.5/CountBank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2._1_Lesson_2._5
+{
+    class CountBank
+    {
+        private int NumCount { get; set; }
+        private decimal Balans { get; set; }
+        private void TakeCount()
+        {
+
+            if (Rand(NumCount) == NumCount)
+            {
+                Rand(NumCount);
+            }
+
+        }
+        public void AddToBalans(int value)
+        {
+            Balans += value;
+        }
+        public void TakeMoney(int value)
+        {
+            if(Balans>value)
+            {
+                Balans -= value;
+            }
+            else
+            {
+                Console.WriteLine("на балансе недостаточно средств");
+            }
+        }
+        private int Rand(int coout)
+        {
+            Random random = new Random();
+            coout = random.Next(1, 1000);
+            NumCount = coout;
+            return coout;
+        }
+        public void TakeInfo()
+        {
+            //Random random = new Random();
+            TakeCount();
+            Console.WriteLine($"ваш номер счёта №{NumCount}, ваш баланс{Balans} ");
+            //TypeCount count = (TypeCount)random.Next(1, 4);
+            //Console.WriteLine($" ваш тип счёта {count}");
+        }
+    }
+}
---
+++ Part 2.1 Lesson 2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Part_2._1_Lesson_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountBank countBank = new CountBank();
+            countBank.AddToBalans(12);
+            countBank.TakeInfo();
+            countBank.AddToBalans(12);
+            countBank.AddToBalans(12);
+            countBank.TakeInfo();
+            countBank.TakeMoney(12);
+            countBank.TakeInfo();
+            countBank.TakeMoney(99);
+            countBank.TakeInfo();
+        }
+    }
+}
